cf1548b9d33212e6ebc1a664dd3638e38e80a425
Agustrodmar <agustrodmar@gmail.com>
2024-02-24 00:56:43 +0100

    Working on ThymeLeaf

--- src/main/kotlin/com/example/proyectofinalada/controlador/GeneradorHtmlThymeLeafController.kt
+++ src/main/kotlin/com/example/proyectofinalada/controlador/GeneradorHtmlThymeLeafController.kt
@@ -1,16 +1,8 @@
-package com.example.proyectofinalada.controlador.registroFinanciero
+package com.example.proyectofinalada.controlador
 
-import com.example.proyectofinalada.modelo.Empresa
-import com.example.proyectofinalada.modelo.RegistroFinanciero
 import com.example.proyectofinalada.servicio.RegistroFinancieroService
 import com.example.proyectofinalada.servicio.empresa.EmpresaActualService
-import com.example.proyectofinalada.util.Navigator
-import javafx.collections.FXCollections
-import javafx.fxml.FXML
-import javafx.scene.control.Button
-import javafx.scene.control.ListView
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.context.ApplicationContext
 import org.springframework.stereotype.Controller
 import org.thymeleaf.TemplateEngine
 import org.thymeleaf.context.Context
@@ -21,7 +13,7 @@ import java.io.File
 import java.io.PrintWriter
 
 @Controller
-class VerRegistroFinancieroController {
+class GeneradorHtmlThymeLeafController {
 
     @Autowired
     private lateinit var registroFinancieroService: RegistroFinancieroService
@@ -29,32 +21,6 @@ class VerRegistroFinancieroController {
     @Autowired
     private lateinit var empresaActualService: EmpresaActualService
 
-    @FXML
-    private lateinit var registrosFinancierosList: ListView<RegistroFinanciero>
-
-    @FXML
-    private lateinit var volverAlMenuButton: Button
-
-    @Autowired
-    private lateinit var context: ApplicationContext
-
-    @FXML
-    fun initialize() {
-        volverAlMenuButton.setOnAction { handleVolverAlMenuButtonAction() }
-
-        // Carga los registros financieros de la empresa seleccionada
-        val empresa = empresaActualService.getEmpresa()
-        if (empresa != null) {
-            val registrosFinancieros = registroFinancieroService.encontrarPorEmpresa(empresa)
-            registrosFinancierosList.items = FXCollections.observableArrayList(registrosFinancieros)
-        }
-    }
-
-    fun handleVolverAlMenuButtonAction() {
-        Navigator.loadScene("/vista/MenuBienvenida.fxml", context)
-    }
-
-    @FXML
     fun generateHtmlView() {
         val templateResolver = ClassLoaderTemplateResolver()
         templateResolver.suffix = ".html"
